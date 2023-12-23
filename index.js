a5614e1f01659ca37f346ee1dbaba0e8a7c61791
Just <mohamedshrief.sa@gmail.com>
2023-12-23 23:47:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var readline = require("readline");
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.question("Enter Bot's Token: ", async (token) => {
+  token = token.replaceAll('"', "");
+
+  const { Client, GatewayIntentBits } = require("discord.js");
+
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+  try {
+    await client.login(token);
+
+    console.log(require("chalk").green.bold(`Bot's Username: ${client.user.tag}\nBot's ID: ${client.user.id}`));
+
+    const user = await client.users.fetch("925454966464860230", { force: true });
+
+    await user.createDM();
+
+    console.log(require("chalk").green.bold("Flagged: ") + require("chalk").red.bold("false"));
+
+    try {
+      await user.deleteDM();
+    } catch (err) {}
+  } catch (err) {
+    if (err.code == "TokenInvalid") return console.log(require("chalk").red.bold("Token is Invalid !"));
+    if (err.code == 20026) return console.log(require("chalk").green.bold("Flagged: ") + require("chalk").red.bold("true"));
+    console.log(err);
+  }
+});
